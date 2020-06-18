fd79d7880bde131afc53b008ce4d2789906123a7
Dominic Taylor <dom.taylor7@gmail.com>
2020-06-18 14:55:03 +0100

    add cakes modules

---
+++ src/app/cakes/cakes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CakesRoutingModule } from './cakes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CakesRoutingModule
+  ]
+})
+export class CakesModule { }
